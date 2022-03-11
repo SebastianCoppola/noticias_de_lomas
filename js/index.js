bde7134d3ea2b2c60c3842b4f11bacb90f7a2205
SebastianCoppola <seba.coppola@hotmail.com>
2022-03-11 18:53:23 -0300

    11marzo actualizacion

--- js/index.js
+++ js/index.js
@@ -15,32 +15,25 @@ fetch('./fecha/fecha.json')
         fecha.innerHTML = res.fecha;
     });
 
-// NOTA-1
-var seccion1 = document.querySelector(".seccion-1");
-var titulo1 = document.querySelector(".titulo-1");
-var subtitulo1 = document.querySelector(".subtitulo-1");
-var autor1 = document.querySelector(".autor-1");
-fetch('./noticias/nota-1.json')
-    .then(res => res.json())
-    .then(res => {
-        seccion1.innerHTML = res.seccion
-        titulo1.innerHTML = res.titulo
-        subtitulo1.innerHTML = res.subtitulo
-        autor1.innerHTML = res.autor
-    });
+//CARGAR AUTOR NOTA 1
 
-// NOTA-2
-const seccion2 = document.querySelector(".seccion-2");
-const titulo2 = document.querySelector(".titulo-2");
-const subtitulo2 = document.querySelector(".subtitulo-2");
-fetch('./noticias/nota-2.json')
-    .then(res => res.json())
-    .then(res => {
-        seccion2.innerHTML = res.seccion
-        titulo2.innerHTML = res.titulo
-        subtitulo2.innerHTML = res.subtitulo
-        autor2.innerHTML = res.autor
-    });
 
+//CARGAR NOTAS
+for(n = 1; n < 20; n++) {
+    
+    const seccion = document.querySelector(`.seccion-${n}`);
+    const titulo = document.querySelector(`.titulo-${n}`);
+    const subtitulo = document.querySelector(`.subtitulo-${n}`);
+    const autor = document.querySelector(`.autor-${n}`);
+    
+    fetch(`./noticias/nota-${n}.json`)
+        .then(res => res.json())
+        .then(res => {
+            seccion.innerHTML = res.seccion
+            titulo.innerHTML = res.titulo
+            subtitulo.innerHTML = res.subtitulo
+            autor.innerHTML = res.autor
+        });
+}
 
 
