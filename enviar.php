6e986777d0f2e0b5d8cba55c23d097fb1ab2b8b9
SebastianCoppola <seba.coppola@hotmail.com>
2022-03-13 11:24:41 -0300

    13marzo update

---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php
+
+$destino = "seba.coppola@hotmail.com";
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono: " . $telefono . "\nMensaje: " . $mensaje;
+
+mail($destino, "Contacto", $contenido);
+header("Location:index.html");
+?>
--- js/index.js
+++ js/index.js
@@ -38,13 +38,62 @@ for(n = 1; n < 20; n++) {
 
 
 //A QUE NOTA VOY
-
-const $nodo = document.querySelectorAll(".noti");
+const $noticia = document.querySelectorAll(".noti");
 // iteras y entregas el índice como argumento
-$nodo.forEach((nodo, index) => {
+$noticia.forEach((noticia, index) => {
     // asignas a cada instancia de tu lista el evento
-    nodo.addEventListener("click", function (e) {
+    noticia.addEventListener("click", function (e) {
         // aquí manipulas el índice del elemento en el arreglo
         localStorage.setItem("noticias",index+1);
+    });
+});
+
+
+//A QUE SECCION VOY NAV
+const $seccionNav = document.querySelectorAll(".seccion-nav");
+// iteras y entregas el índice como argumento
+$seccionNav.forEach((seccionNav, index) => {
+    // asignas a cada instancia de tu lista el evento
+    seccionNav.addEventListener("click", function (e) {
+        // aquí manipulas el índice del elemento en el arreglo
+        if(index==0){
+            localStorage.setItem("seccion","Sociedad");
+        } else if(index==1){
+            localStorage.setItem("seccion","Política");
+        } else if(index==2){
+            localStorage.setItem("seccion","Deportes");
+        } else if(index==3){
+            localStorage.setItem("seccion","Gastronomía");
+        } else if(index==4){
+            localStorage.setItem("seccion","Cultura");
+        } else if(index==5){
+            localStorage.setItem("seccion","Opinión");
+        } else if(index==6){
+            localStorage.setItem("seccion","Historia Regional");
+        }
+    });
+});
+//A QUE SECCION VOY FOOTER
+const $seccionFoot = document.querySelectorAll(".seccion-footer");
+// iteras y entregas el índice como argumento
+$seccionFoot.forEach((seccionFoot, index) => {
+    // asignas a cada instancia de tu lista el evento
+    seccionFoot.addEventListener("click", function (e) {
+        // aquí manipulas el índice del elemento en el arreglo
+        if(index==0){
+            localStorage.setItem("seccion","Sociedad");
+        } else if(index==1){
+            localStorage.setItem("seccion","Política");
+        } else if(index==2){
+            localStorage.setItem("seccion","Deportes");
+        } else if(index==3){
+            localStorage.setItem("seccion","Gastronomía");
+        } else if(index==4){
+            localStorage.setItem("seccion","Cultura");
+        } else if(index==5){
+            localStorage.setItem("seccion","Opinión");
+        } else if(index==6){
+            localStorage.setItem("seccion","Historia Regional");
+        }
     });
 });
